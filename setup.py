55a30554fd8ff35c61f1ef84f20cec10cadbd1dd
Kukushkin Alexander <alex@alexkuk.ru>
2020-03-19 17:01:50 +0300

    Up tags, desc

--- setup.py
+++ setup.py
@@ -1,26 +1,35 @@
-# coding: utf-8
-from __future__ import unicode_literals
 
 from setuptools import setup, find_packages
+
+
+with open('README.md') as file:
+    description = file.read()
 
 
 setup(
     name='razdel',
     version='0.4.0',
+
     description='Splits russian text into tokens, sentences, section. Rule-based',
+    long_description=description,
+    long_description_content_type='text/markdown',
+
     url='https://github.com/natasha/razdel',
     author='Alexander Kukushkin',
     author_email='alex@alexkuk.ru',
     license='MIT',
+
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3',
     ],
-    keywords='natural language processing, russian, token, sentence',
+    keywords='nlp, natural language processing, russian, token, sentence, tokenize',
+
     packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'razdel-ctl=razdel.tests.ctl:main'
+        ],
+    },
+    install_requires=[]
 )
